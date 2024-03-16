543cd3a88da7d1fef88fbbab019edcd96a209bf8
Piyush Jain <simran.dhaliwal@shephertz.com>
2024-03-16 12:50:33 +0530

    changed the code

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const SibApiV3Sdk = require('@getbrevo/brevo');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -22,9 +24,11 @@ app.post('/send-mail', async (req, res) => {
     try {
         let user_email = req.body.user_email;
         let user_name = req.body.user_name;
+        let phone_number = req.body.phone_number;
+        let course_name = req.body.course_name;
+        let city = req.body.city;
 
         console.log(user_email, user_name);
-
         
 		if (user_email === undefined || user_email === '') {
 			return res.status(422).json({
@@ -34,6 +38,27 @@ app.post('/send-mail', async (req, res) => {
 		}
 
 		if (user_name === undefined || user_name === '') {
+			return res.status(422).json({
+				status: false,
+				message: 'User Name is required.',
+			})
+		}
+
+        if (phone_number === undefined || phone_number === '') {
+			return res.status(422).json({
+				status: false,
+				message: 'User Name is required.',
+			})
+		}
+
+        if (course_name === undefined || course_name === '') {
+			return res.status(422).json({
+				status: false,
+				message: 'User Name is required.',
+			})
+		}
+
+        if (city === undefined || city === '') {
 			return res.status(422).json({
 				status: false,
 				message: 'User Name is required.',
@@ -53,15 +78,15 @@ app.post('/send-mail', async (req, res) => {
         let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
         let apiKey = apiInstance.authentications['apiKey'];
-        apiKey.apiKey = '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';
+        apiKey.apiKey = process.env.MAIL_KEY;
 
         let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); 
 
-        sendSmtpEmail.subject = "My {{params.subject}}";
-        sendSmtpEmail.htmlContent = "<html><body><h1>This is my first transactional email {{params.parameter}}</h1></body></html>";
-        sendSmtpEmail.sender = {"name":"John Doe","email":"piyush.shephertz@gmail.com"};
+        sendSmtpEmail.subject = "{{params.subject}}";
+        sendSmtpEmail.htmlContent = `<html><body><p>Hello,</p><p>Below are the user Details who have submitted the form</p><div><p>User Name: ${user_name}</p><p>User Email: ${user_email}</p><p>Phone Number: ${phone_number}</p><p>Course Name: ${course_name}</p><p>City: ${city}</p></div><div>Thank You.</div></body></html>`;
+        sendSmtpEmail.sender = {"name":"BRAIN TECH Education & Placement","email":"piyush.shephertz@gmail.com"};
         sendSmtpEmail.to = [{"email": user_email,"name": user_name}];
-        sendSmtpEmail.params = {"parameter":"My param value","subject": user_name};
+        sendSmtpEmail.params = {"parameter":"My param value","subject": `BRAIN TECH - User Details (${user_name})`};
 
         let data = await apiInstance.sendTransacEmail(sendSmtpEmail);
 
